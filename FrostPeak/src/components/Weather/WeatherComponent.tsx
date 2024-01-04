c5414e67c88e79ffc7b07c76cbd2faf44177355b
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-01-04 18:19:17 +0100

    More design work done on homepage

--- FrostPeak/src/components/Weather/WeatherComponent.tsx
+++ FrostPeak/src/components/Weather/WeatherComponent.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import fetchWeatherInfo from '../../services/weather/weatherService';
+import { TiWeatherSnow, TiWeatherCloudy, TiWeatherWindyCloudy } from "react-icons/ti";
+import { FaTemperatureLow } from "react-icons/fa";
 
 const WeatherComponent = () => {
     const [weatherInfo, setWeatherInfo] = useState<{
@@ -18,7 +20,8 @@ const WeatherComponent = () => {
         const data = await fetchWeatherInfo();
         setWeatherInfo(data);
       } catch (error) {
-        // Handle error if needed
+        console.log("Error with weather info");
+        
       }
     };
 
@@ -26,14 +29,13 @@ const WeatherComponent = () => {
   }, []);
 
   return (
-    <div>
+    <div className="bg-black w-80 mx-auto p-4 rounded-3xl">
       {weatherInfo && (
         <div>
-          <h2 className="font-bold">Current Weather</h2>
-          <p>Temperature: {weatherInfo.current.temperature2m} °C</p>
-          <p>Snowfall: {weatherInfo.current.snowfall} mm</p>
-          <p>Wind Speed: {weatherInfo.current.windSpeed10m} m/s</p>
-          <p>Wind Direction: {weatherInfo.current.windDirection10m} degrees</p>
+          <h2 className="font-bold text-white"><TiWeatherCloudy className="inline-block mr-2" /> Current Weather</h2>
+          <p className="text-white"><FaTemperatureLow className="inline-block mr-2" /> Temperature: {weatherInfo.current.temperature2m} °C</p>
+          <p className="text-white"><TiWeatherSnow className="inline-block mr-2" /> Snowfall: {weatherInfo.current.snowfall} mm</p>
+          <p className="text-white"><TiWeatherWindyCloudy className="inline-block mr-2" /> Wind Speed: {weatherInfo.current.windSpeed10m} m/s</p>
         </div>
       )}
     </div>
