a6917860541b9725f798a151b277e28677c83d82
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-01-11 16:55:42 +0100

    Starting to implement the places API figuration

--- FrostPeak/src/services/places/placeService.ts
+++ FrostPeak/src/services/places/placeService.ts
@@ -1,7 +1,7 @@
 export const initializePlacesAPI = () => {
     if (window.google && window.google.maps && window.google.maps.places) {
-      console.log('Places API initialized');
+      console.log("Places API is initialized");
     } else {
-      console.error('Places API is not available');
+      console.error("Places API is not initialized");
     }
 };
