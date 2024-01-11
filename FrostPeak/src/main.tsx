ce3280eefae3861c424600c4b39b8323d56676df
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-01-11 15:36:25 +0100

    Installed necessary dependencies for API calls, figured out google map environment + got API key, declared it in .env and setup the mapService for API calling.

--- FrostPeak/src/main.tsx
+++ FrostPeak/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './input.css'
 
+if (import.meta.env.MODE === 'development') { // For Maps and Places API key.
+    import('dotenv').then((dotenv) => dotenv.config());
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
---
+++ FrostPeak/src/services/maps/mapService.ts
@@ -0,0 +1,36 @@
+declare global { // Declare structure of the google object in the window interface.
+    interface Window {
+      google: {
+        maps: {
+           Map: new (element: HTMLElement, options: google.maps.MapOptions) => google.maps.Map;
+        };
+      };
+    }
+  }
+  
+const loadGoogleScript = (callback: () => void) => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+    script.onload = callback;
+};
+  
+
+export const initMap = () => { // Initialize google maps
+    const mapElement = document.getElementById('map');
+
+    if (mapElement) {
+      const map = new window.google.maps.Map(mapElement, { // Create a new Google Map instance
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    }
+};
+  
+export const loadGoogleMap = () => {
+    loadGoogleScript(() => {
+      initMap();
+    });
+};
