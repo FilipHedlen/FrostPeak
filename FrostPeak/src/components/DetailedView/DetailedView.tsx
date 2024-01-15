945d2bd70be9a3ca064b35d56c799508fb060549
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-01-15 13:34:14 +0100

    Simplifying the logic for the locations page, removed the marker for specific locations for now

--- FrostPeak/src/components/DetailedView/DetailedView.tsx
+++ FrostPeak/src/components/DetailedView/DetailedView.tsx
@@ -2,29 +2,31 @@ import { useEffect } from "react";
 import MapContainer from "../Map/MapComponent";
 
 interface DetailedViewProps {
-    place: google.maps.places.PlaceResult;
-  }
-  
-  const DetailedViewComponent: React.FC<DetailedViewProps> = ({ place }) => {
-    useEffect(() => {
-        new window.google.maps.Marker({ // Marker to specify exactly where the position of the location is
-          position: place.geometry?.location,
-          icon: {
-            path: window.google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-            scale: 5,
-            strokeColor: 'red',
-          },
-        });
-      }, [place]);
+  place: google.maps.places.PlaceResult;
+}
 
-    return (
-      <div>
-        <h2>{place.name}</h2>
-        <p>{place.adr_address}</p>
-        <p>{place.rating}</p>
-        <MapContainer center={place.geometry?.location || { lat: 0, lng: 0 }} markerPosition={place.geometry?.location} />
-      </div>
-    );
-  };
-  
-  export default DetailedViewComponent;
+const DetailedViewComponent: React.FC<DetailedViewProps> = ({ place }) => {
+  useEffect(() => {
+    if (place.geometry) {
+      const mapElement = document.getElementById('map');
+
+      if (mapElement) {
+        new window.google.maps.Map(mapElement, { // Centers in on the location that the user just clicked
+          center: place.geometry.location,
+          zoom: 15,
+        });
+      }
+    }
+  }, [place]);
+
+  return (
+    <div>
+      <h2>{place.name}</h2>
+      <p>{place.adr_address}</p>
+      <p>{place.rating}</p>
+      <MapContainer center={place.geometry?.location || { lat: 0, lng: 0 }} />
+    </div>
+  );
+};
+
+export default DetailedViewComponent;
--- FrostPeak/src/components/Map/MapComponent.tsx
+++ FrostPeak/src/components/Map/MapComponent.tsx
@@ -1,12 +1,11 @@
-import { GoogleMap, Marker } from '@react-google-maps/api';
+import { GoogleMap } from '@react-google-maps/api';
 import useGoogleMaps from '../../services/maps/mapService';
 
 interface MapContainerProps {
-    center: google.maps.LatLng | { lat: number; lng: number };
-    markerPosition?: google.maps.LatLng| undefined;
+  center?: google.maps.LatLng | { lat: 0, lng: 0 };
 }
 
-const MapContainer: React.FC<MapContainerProps> = ({ center, markerPosition }) => {
+const MapContainer: React.FC<MapContainerProps> = ({ center }) => {
   const { isLoaded } = useGoogleMaps();
 
   if (!isLoaded) {
@@ -14,15 +13,13 @@ const MapContainer: React.FC<MapContainerProps> = ({ center, markerPosition }) =
   }
 
   const mapOptions = {
-    center: center as google.maps.LatLngLiteral,
+    center,
     zoom: 15,
   };
 
   return (
-    <div style={{ height: '400px', width: '400px', margin: '0 auto' }}>
-      <GoogleMap mapContainerStyle={{ height: '100%', width: '100%', borderRadius: '5%' }} {...mapOptions}>
-        {markerPosition && <Marker position={markerPosition} icon={{ path: window.google.maps.SymbolPath.BACKWARD_CLOSED_ARROW, scale: 5, strokeColor: 'red' }} />}
-      </GoogleMap>
+    <div style={{ height: '400px', width: '400px', margin: "0 auto"}}> 
+      <GoogleMap mapContainerStyle={{ height: '100%', width: '100%', borderRadius: '5%' }} {...mapOptions} />
     </div>
   );
 };
