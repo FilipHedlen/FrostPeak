de51cb677bfe97fbc4cbcd96df737173f07ad3a9
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-01-18 16:07:19 +0100

    Installed a react zoom package, implemented it with my slope map

--- FrostPeak/src/components/SkiSlopes/SkiSlopes.tsx
+++ FrostPeak/src/components/SkiSlopes/SkiSlopes.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
 
 const SkiSlopes = () => {
   return (
-    <div>SkiSlopes</div>
+    <div>
+      <Zoom>
+        <img src="/ramund-slopes.jpg" alt="Ramundbergets ski slope map" className="rounded-md" />
+      </Zoom>
+    </div>
   )
 }
 
