2483663538dd362417c8fdb705eb019d6fabfd36
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-01-25 15:42:10 +0100

    Some designwork to the list toggle

--- FrostPeak/src/components/SlopeLiftToggle/SlopeLiftToggle.tsx
+++ FrostPeak/src/components/SlopeLiftToggle/SlopeLiftToggle.tsx
@@ -1,17 +1,36 @@
 import { useState } from 'react';
 import SlopeList from '../Slopes/SlopeList/SlopeList';
 import LiftList from '../SkiLifts/LiftList/LiftList';
+import { TbAerialLift } from "react-icons/tb";
+import { FaPersonSkiing } from 'react-icons/fa6';
 
 const SlopeLiftToggle = () => {
-  const [selectedList, setSelectedList] = useState("slopes"); // Slopelist is the default
+  const [selectedList, setSelectedList] = useState("slopes"); // Slopelist is the default state
 
-  const handleToggle = () => {
-    setSelectedList((prev) => (prev === "slopes" ? "lifts" : "slopes"));
+  const handleToggle = (listType: string) => {
+    setSelectedList(listType);
   };
 
   return (
     <div>
-      <button onClick={handleToggle}>Toggle Slopes/Lifts</button>
+      <button
+        onClick={() => handleToggle("slopes")}
+        className={`
+          ${
+            selectedList === "slopes" ? "bg-white" : "bg-gray-400"
+          } px-4 py-2 text-lg mr-4 border-none cursor-pointer inline-block`}
+      >
+        Slopes <FaPersonSkiing className="ml-2" />
+      </button>
+      <button
+        onClick={() => handleToggle("lifts")}
+        className={`
+          ${
+            selectedList === "lifts" ? "bg-white" : "bg-gray-400"
+          } px-4 py-2 text-lg border-none cursor-pointer inline-block`}
+      >
+        Lifts <TbAerialLift className="ml-2" />
+      </button>
       {selectedList === "slopes" ? <SlopeList /> : <LiftList />}
     </div>
   );
