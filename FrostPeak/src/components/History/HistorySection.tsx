8643a5d82b9a8b944766cc3ac0b1fa79e6d8a8f2
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-01-29 16:07:42 +0100

    More history added, more styling work done

--- FrostPeak/src/components/History/HistorySection.tsx
+++ FrostPeak/src/components/History/HistorySection.tsx
@@ -16,7 +16,7 @@ const HistorySection = () => {
       >
         <article>
           <h2 className="text-2xl font-playfair font-semibold mb-4 text-black">Ramundbergets history</h2>
-          <p className="text-grey font-poppins">
+          <p className="text-grey font-poppins leading-relaxed pt-6">
             The history of Ramundberget traces back to 1934, 
             when skiing students stayed in the tourist association's cottage Girtibauna, 
             6 kilometers northwest of Ramundberget.
@@ -30,11 +30,18 @@ const HistorySection = () => {
             Despite the lack of beds, Agaton negotiated for ten beds on credit from his brother-in-law in Funäsdalen,
             and the commercial activity in Ramundberget started. 
           </p>
-          <p className="text-grey">
-
+          <p className="text-grey font-poppins leading-relaxed pt-6">
+            Ramundberget in Härjedalen is owned by the Flacké family since 1993, 
+            with Philip Flacké as chairman of the board.
+            The resort marked its 75th anniversary in 2011, which makes for its 88th in 2024.
+            Noting achievements include 5000 employees taht have passed through the resort,
+            55 million skier descents, 
+            and snow preparations covering areas equivalent to 374,000 football fields since 1936.
+            The resort's history includes milestones such as the introduction of the telephone in 1937, 
+            the completion of the road from Bruksvallarna in 1949, and the construction of the first lift in 1958.
           </p>
           <div className="text-lightgrey font-nunito text-sm mt-4">
-            <p>By Håkan Persson</p>
+            <p>Cited by Håkan Persson</p>
             <p>Published on April 5, 2011</p>
             <p>Source:
                 <a 
