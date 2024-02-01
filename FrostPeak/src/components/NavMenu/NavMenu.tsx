715d50d0719c80a11edee06371c99223b8827b02
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-02-01 15:02:55 +0100

    Revamped the entire nav menu, removed useless functions for routing

--- FrostPeak/src/components/NavMenu/NavMenu.tsx
+++ FrostPeak/src/components/NavMenu/NavMenu.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const NavMenu: React.FC = () => {
+const NavMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
@@ -13,13 +12,8 @@ const NavMenu: React.FC = () => {
     setIsOpen(false);
   };
 
-  const handleLinkClick = (path: string) => {
-    closeMenu();
-    navigate(path);
-  };
-
   return (
-    <nav className="lg:flex lg:items-center lg:w-auto lg:space-x-4" style={{ zIndex: 999 }}>
+    <nav className="lg:flex lg:items-center lg:w-auto lg:space-x-4">
       <div className="flex items-center justify-between">
         <button
           onClick={toggleMenu}
@@ -31,14 +25,9 @@ const NavMenu: React.FC = () => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              className="h-6 w-6"
+              className="h-8 w-8"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
             </svg>
           ) : (
             <svg
@@ -46,47 +35,81 @@ const NavMenu: React.FC = () => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              className="h-6 w-6"
+              className="h-8 w-8"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           )}
         </button>
       </div>
-      <div className={`${isOpen ? 'block' : 'hidden'} lg:flex lg:items-center lg:w-auto`}>
-        <ul className="lg:flex lg:space-x-4 font-nunito">
-          <li>
-            <Link to="/" onClick={() => handleLinkClick('/')}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/locations" onClick={() => handleLinkClick('/locations')}>
-              Locations
-            </Link>
-          </li>
-          <li>
-            <Link to="/slopes" onClick={() => handleLinkClick('/slopes')}>
-              Slopes
-            </Link>
-          </li>
-          <li>
-            <Link to="/info" onClick={() => handleLinkClick('/info')}>
-              Information
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" onClick={() => handleLinkClick('/contact')}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {isOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="bg-white p-24 rounded-lg shadow-md transition-all duration-2500 ease-in-out transform scale-100 opacity-100 z-50">
+            <ul className="flex flex-col space-y-4 font-poppins text-2xl text-black">
+              <li>
+                <Link 
+                to="/" 
+                onClick={() => {
+                    closeMenu();
+                }}
+                className="hover:border-b-2 border-black transition duration-100"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link 
+                to="/locations" 
+                onClick={() => {
+                    closeMenu();
+                }}
+                className="hover:border-b-2 border-black transition duration-100"
+                >
+                  Locations
+                </Link>
+              </li>
+              <li>
+                <Link
+                to="/slopes" 
+                onClick={() => {
+                    closeMenu();
+                }}
+                className="hover:border-b-2 border-black transition duration-100"
+                >
+                  Slopes
+                </Link>
+              </li>
+              <li>
+                <Link 
+                to="/info" 
+                onClick={() => {
+                    closeMenu();
+                }}
+                className="hover:border-b-2 border-black transition duration-100"
+                >
+                  Information
+                </Link>
+              </li>
+              <li>
+                <Link 
+                to="/contact" 
+                onClick={() => {
+                    closeMenu();
+                }}
+                className="hover:border-b-2 border-black transition duration-100"
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div
+            className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50"
+            onClick={closeMenu}
+          >
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
