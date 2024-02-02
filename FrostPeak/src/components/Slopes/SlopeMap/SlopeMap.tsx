017685ccd03f8229a10bbc81bea29794693ce58a
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-02-02 16:35:26 +0100

    Small styling change to slope map

--- FrostPeak/src/components/Slopes/SlopeMap/SlopeMap.tsx
+++ FrostPeak/src/components/Slopes/SlopeMap/SlopeMap.tsx
@@ -3,12 +3,12 @@ import 'react-medium-image-zoom/dist/styles.css';
 
 const SlopeMap = () => {
   return (
-      <div className="mb-5 mr-5">
+      <div className="mb-5 ml-5 mr-5">
         <Zoom>
           <img
             src="/ramund-slopes.jpg"
             alt="Ramundbergets ski slope map"
-            className="rounded-md w-[50%] h-auto mx-auto"
+            className="rounded-md md:w-[50%] sm:w-[80%] h-auto mx-auto"
           />
         </Zoom>
     </div>
