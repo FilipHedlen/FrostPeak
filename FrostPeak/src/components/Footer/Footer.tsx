d6b23067c1e43e0ccbe9875c93e563b47ca0c02d
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-02-06 13:56:43 +0100

    Small change to footer

--- FrostPeak/src/components/Footer/Footer.tsx
+++ FrostPeak/src/components/Footer/Footer.tsx
@@ -2,13 +2,11 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-darkblue text-white font-nunito font-bold p-4 fixed bottom-0 w-full flex justify-around">
-      <div>Made by Filip Hedlén | © FrostPeak Explorer 2024</div>
-      <div>
-        <Link to="/contact" className="text-white">
-          Contact Us
-        </Link>
-      </div>
+    <footer className="bg-darkblue text-white font-poppins font-bold p-6 fixed bottom-0 w-full flex justify-between">
+      <p>© FrostPeak Explorer 2024</p>
+      <Link to="/contact" className="text-white">
+        Contact Us
+      </Link>
     </footer>
   );
 };
