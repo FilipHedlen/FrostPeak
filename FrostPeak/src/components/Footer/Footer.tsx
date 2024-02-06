10ac66bfb689a6db96c0d04b74f9a43094120570
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2024-02-06 14:22:54 +0100

    Small footer change again

--- FrostPeak/src/components/Footer/Footer.tsx
+++ FrostPeak/src/components/Footer/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
     <footer className="bg-darkblue text-white font-poppins font-bold p-6 fixed bottom-0 w-full flex justify-between">
       <p>© FrostPeak Explorer 2024</p>
       <div>
-      <Link to="/about" className="text-white mr-6 hover:bg-cherryred">
-        About
-      </Link>
-      <Link to="/contact" className="text-white hover:bg-cherryred">
-        Contact
-      </Link>
+        <Link to="/about" className="text-white mr-6 hover:bg-cherryred">
+          About
+        </Link>
+        <Link to="/contact" className="text-white hover:bg-cherryred">
+          Contact
+        </Link>
       </div>
     </footer>
   );
